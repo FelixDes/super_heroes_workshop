3e7ecff05df7cd8ea104ce05523811390e415451
felix <vip.zatebe@mail.ru>
2023-01-08 13:44:02 +0300

    Reconfiguration of maven modules

--- rest_villains/src/main/java/com/example/rest_villains/data/VillainRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.rest_villains.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VillainRepository extends JpaRepository<Villain, Long> {
-}
