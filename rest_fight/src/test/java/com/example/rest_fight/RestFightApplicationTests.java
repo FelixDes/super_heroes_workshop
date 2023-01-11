d97d1481b602b89d75f2b68a9e1a543a98a68062
felix <vip.zatebe@mail.ru>
2023-01-11 21:18:32 +0300

    Hero REST

---
+++ rest_fight/src/test/java/com/example/rest_fight/RestFightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_fight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
