ac1d7dd90e8bbd9a947bbd985ac25f018668a448
felix <vip.zatebe@mail.ru>
2023-01-27 18:01:08 +0300

    Add base structure of statistics

---
+++ statistics/src/test/java/com/example/statistics/StatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
