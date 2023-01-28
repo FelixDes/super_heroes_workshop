9296baa2c5997583566e2839a2c5be18e788a344
felix <vip.zatebe@mail.ru>
2023-01-28 19:47:52 +0300

    Add kafka config for global statistics listeners

--- statistics/src/main/java/com/example/statistics/kafka/config/KafkaListenerFightToDoubleConfig.java
+++ statistics/src/main/java/com/example/statistics/kafka/config/KafkaListenerFightToDoubleConfig.java
@@ -1,59 +1,35 @@
-package com.example.statistics.kafka;
+package com.example.statistics.kafka.config;
 
 import com.example.statistics.data.Fight;
-import com.example.statistics.serialization.FightDeserializer;
+import com.example.statistics.data.serialization.FightDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.DoubleDeserializer;
 import org.apache.kafka.common.serialization.DoubleSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
-import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ConsumerProperties;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConfig {
+public class KafkaListenerFightToDoubleConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
-    private final String offset = "earliest";
-
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String offset;
     @Bean
-    public ConsumerFactory<String, Double> kafkaConsumerFactoryDouble() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class);
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "team_stats_group");
-
-        return new DefaultKafkaConsumerFactory<>(props);
-    }
-
-    @Bean(name = "kafkaListenerContainerFactoryDouble")
-    public ConcurrentKafkaListenerContainerFactory<String, Double> kafkaListenerContainerFactoryDouble() {
-        ConcurrentKafkaListenerContainerFactory<String, Double> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(kafkaConsumerFactoryDouble());
-        return factory;
-    }
-
-    @Bean
-    public ConsumerFactory<String, Fight> kafkaConsumerFactoryFight() {
+    public ConsumerFactory<String, Fight> kafkaConsumerFactoryFightTeamStats() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, FightDeserializer.class);
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "fight_group");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "fight_team_stats_group");
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
@@ -76,7 +52,7 @@ public class KafkaConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Fight> kafkaListenerContainerFactoryFightToDouble() {
         ConcurrentKafkaListenerContainerFactory<String, Fight> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(kafkaConsumerFactoryFight());
+        factory.setConsumerFactory(kafkaConsumerFactoryFightTeamStats());
         factory.setReplyTemplate(kafkaTemplateDouble());
         return factory;
     }
---
+++ statistics/src/main/java/com/example/statistics/kafka/config/KafkaListenerFightToScoresDTOConfig.java
@@ -0,0 +1,62 @@
+package com.example.statistics.kafka.config;
+
+import com.example.statistics.data.Fight;
+import com.example.statistics.data.ScoreListDTO;
+import com.example.statistics.data.serialization.FightDeserializer;
+import com.example.statistics.data.serialization.ScoreListDTOSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaListenerFightToScoresDTOConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String offset;
+
+    @Bean
+    public ConsumerFactory<String, Fight> kafkaConsumerFactoryFightWinnerStats() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, FightDeserializer.class);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offset);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "fight_winner_stats_group");
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ProducerFactory<String, ScoreListDTO> producerFactoryScoreDTO() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ScoreListDTOSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, ScoreListDTO> kafkaTemplateScoreDTO() {
+        return new KafkaTemplate<>(producerFactoryScoreDTO());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Fight> kafkaListenerContainerFactoryFightToScoreDTO() {
+        ConcurrentKafkaListenerContainerFactory<String, Fight> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(kafkaConsumerFactoryFightWinnerStats());
+        factory.setReplyTemplate(kafkaTemplateScoreDTO());
+        return factory;
+    }
+}
