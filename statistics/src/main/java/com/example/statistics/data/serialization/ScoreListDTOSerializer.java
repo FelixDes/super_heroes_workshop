09ff6659f11f561bc7e45e6c4b4c014055724175
felix <vip.zatebe@mail.ru>
2023-01-28 19:51:20 +0300

    Add ScoreListDTO serialization

---
+++ statistics/src/main/java/com/example/statistics/data/serialization/ScoreListDTOSerializer.java
@@ -0,0 +1,21 @@
+package com.example.statistics.data.serialization;
+
+import com.example.statistics.data.ScoreListDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class ScoreListDTOSerializer implements Serializer<ScoreListDTO> {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public byte[] serialize(String topic, ScoreListDTO data) {
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (JsonProcessingException e) {
+            throw new SerializationException("Error during serialization from ScoreListDTO to byte[]", e);
+        }
+    }
+}
