2917268b7048f740e9b6cee92830027029c8077b
felix <vip.zatebe@mail.ru>
2023-03-05 13:42:28 +0300

    Disable test in run script

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ cd ./rest_villain
 docker-compose rm -f
 docker-compose pull
 docker-compose down --remove-orphans
-mvn clean install
+mvn clean install -Dmaven.test.skip
 docker-compose up --build -d
 cd ../
 
@@ -10,11 +10,18 @@ cd ./rest_hero
 docker-compose rm -f
 docker-compose pull
 docker-compose down --remove-orphans
-mvn clean install
+mvn clean install -Dmaven.test.skip
 docker-compose up --build -d
 cd ../
 
 cd ./kafka
+docker-compose rm -f
+docker-compose pull
+docker-compose down --remove-orphans
+docker-compose up --build -d
+cd ../
+
+cd ./keycloak
 docker-compose rm -f
 docker-compose pull
 docker-compose down --remove-orphans
