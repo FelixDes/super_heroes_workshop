1553ecd1c395f30d782fd5cf45fe23849ecfd117
felix <vip.zatebe@mail.ru>
2023-03-10 23:46:24 +0300

    Containerization

---
+++ rest_fight/db_container/init.sql
@@ -0,0 +1,1 @@
+create database fights_db;
--- run.sh
+++ run.sh
@@ -1,3 +1,30 @@
+docker network create keycloak
+docker network create backend
+docker network create kafka
+docker network create frontend
+
+cd ./kafka
+docker-compose rm -f
+docker-compose pull
+docker-compose down --remove-orphans
+docker-compose up --build -d
+cd ../
+
+cd ./keycloak
+docker-compose rm -f
+docker-compose pull
+docker-compose down --remove-orphans
+docker-compose up --build -d
+cd ../
+
+#cd ./rest_fight
+#docker-compose rm -f
+#docker-compose pull
+#docker-compose down --remove-orphans
+#mvn clean install -Dmaven.test.skip
+#docker-compose up --build -d
+#cd ../
+
 cd ./rest_villain
 docker-compose rm -f
 docker-compose pull
@@ -14,16 +41,9 @@ mvn clean install -Dmaven.test.skip
 docker-compose up --build -d
 cd ../
 
-cd ./kafka
-docker-compose rm -f
-docker-compose pull
-docker-compose down --remove-orphans
-docker-compose up --build -d
-cd ../
-
-cd ./keycloak
-docker-compose rm -f
-docker-compose pull
-docker-compose down --remove-orphans
-docker-compose up --build -d
-cd ../
+#cd ./frontend
+#docker-compose rm -f
+#docker-compose pull
+#docker-compose down --remove-orphans
+#docker-compose up --build -d
+#cd ../
